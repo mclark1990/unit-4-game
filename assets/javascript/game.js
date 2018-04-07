41471753ac6d85643f7902d0e3513e69316d5351
mclark1990 <mjc8875@gmail.com>
2018-04-06 23:21:56 -0400

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,90 @@
+
+
+
+
+$(document).ready(function(){
+
+//variables
+
+let wins = 0
+let losses = 0
+let totalScore = 0;
+let randomScore;
+let myId = 0;
+
+//function to run at start and reset of game
+function resetStartGame(){
+    
+ //array for gem images   
+ let images = ['assets/images/images-1.png', 'assets/images/images-2.png', 'assets/images/images.png', 'assets/images/small_15735.png']
+
+    $("#buttons").empty()
+    
+   //creates random number for user to match and diplays it in #random-number div
+    randomScore = 
+    Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    console.log(randomScore);
+    $("#random-number").html("Random Number: " + randomScore);
+
+
+    //for loop to create random values for buttons and appends the values to buttons
+    //loops images to each button
+ for (i = 0; i < 4; i++) {
+
+  let randomGem = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+  console.log(randomGem);
+  
+  let gem = $("<button>");
+  gem.addClass("gems");
+  gem.attr("data-random", randomGem);
+  gem.attr("id", "gem_" + myId++)
+  gem.css({"background-image":"url('" + images[i] + "')"});
+  $("#buttons").append(gem);   
+
+
+} //end function resetStartGame
+
+};
+
+//call resetStartGame function
+resetStartGame()
+
+
+//runs function on click to convert data-random attribute to integer
+$(document).on("click", ".gems", function(){
+ let number =  parseInt($(this).attr("data-random"));
+
+console.log(number);
+
+//sets totalScore to sum the number for every click
+totalScore += number;
+$("#total-score").html("Your Total Score: " + totalScore);
+//console.log(totalScore);
+
+
+//if statements for winning and losing
+if(randomScore === totalScore){
+    alert("WINNER!!");
+    wins++;
+    totalScore = 0;
+    $("#total-score").html("Your Total Score: " + totalScore);
+    $("#para-win").html("Wins: " + wins);
+   resetStartGame();
+  
+}
+
+if(totalScore > randomScore){
+    alert("Sorry, you lost!!");
+    losses++;
+    totalScore = 0;
+    $("#total-score").html("Your Total Score: " + totalScore);
+    $("#para-loss").html("Losses: " + losses);
+    resetStartGame()
+}
+//end if statements
+
+});
+
+
+
+});
